f8af5728ebe7a291362e96a7c40fd390a3a35583
jdavidhermoso <jdhermoso@farmadosis.es>
2016-08-27 09:14:53 +0200

    The end

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $("#buttons").on('click','#findBtn',function() {
-        var jQueryObj = $('li');
+        //Repeated code!
 
+        var jQueryObj = $('li');
         //Selector
         //$('ul.models').find('.car-model').css('backgroundColor','red');
         //Element
@@ -11,7 +12,29 @@ $(document).ready(function() {
         $('ul.models').find(jQueryObj).css('backgroundColor','red');
     });
 
-    $("#buttons").on('click','#filterBtn',function() {
+    $("#buttons").on('click','#filterBtn', function() {
+        //I don't thik We need to check the selector, Element, or selection (jQueryObj) mode working.
+        //Let's write the function arg mode only
+/*
+        Get the brand ul with the id specified on the input
+        var brandN = $("#brand-input").val();
+        $( ".brand" ).filter(function( index ) {
+                var carBrandId = $(this).data('brandId');
+                return carBrandId == brandN;
+            }).css('backgroundColor','red');*/
+
+
+        //So... What about if I whant to get the .model li for colouring, but comparing with brand elements? Repeated code!
+       /* var brandN = $("#brand-input").val();
+        $( ".brand" ).filter(function( index ) {
+            var carBrandId = $(this).data('brandId');
+            return carBrandId == brandN;
+        }).find('.car-model').css('backgroundColor','red');*/
+
+        //Even car models elements!
+        $( ".brand .car-model" ).filter(function( index ) {
+            return (index+1) % 2 === 0
+        }).css('backgroundColor','red');
 
     });
 });
